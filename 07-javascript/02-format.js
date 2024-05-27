bcf155aeb2a7c9952fc0f5d61faba1e0f3d69081
datdinh7719 <datdinh@pdx.edu>
2024-05-27 16:48:59 -0700

    Lab 7 Javascript

--- 07-javascript/02-format.js
+++ 07-javascript/02-format.js
@@ -4,7 +4,17 @@ const formatItems = (input) => {
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 
-  return input;
+  // for (let i = 0; i < input.length; i++) {
+  //   console.log(input[i]);
+  // }
+
+  let result = [];
+
+  for (let item of input) {
+    result.push(item.toFixed(3));
+  }
+
+  return result.join(", ");
 };
 
 console.log(formatItems([1, 4, 17, 26, 41]));
